7212396c329e3c68c4ac182c4749b93857011729
LE PHUOC DAT <deltora1st@gmail.com>
2026-09-15 00:02:09 +0700

    Add Github Pages

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PFFDA-FE/',
+
   plugins: [
     vue(),
     vueDevTools(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
